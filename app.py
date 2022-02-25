3474fa2257ca9b9da1355377c3b4be55a3fa6195
Tbsc <Tbsc@users.noreply.github.com>
2022-02-25 17:53:45 +0200

    Have Flask run also by directly calling the script

--- app.py
+++ app.py
@@ -44,3 +44,7 @@ def quiz():
 @app.route('/favicon.ico')
 def favicon():
     return flask.send_from_directory(os.path.join(app.root_path, 'static/units/sections'), 'operations.png')
+
+
+if __name__ == '__main__':
+    app.run()
