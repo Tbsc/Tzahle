fb185778508a871b076d915d989e2895d8f0cfe2
Tbsc <Tbsc@users.noreply.github.com>
2022-02-26 18:20:27 +0200

    Improve quiz robustness and completeness

--- app.py
+++ app.py
@@ -33,6 +33,14 @@ def alt_main(tag_path=''):
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
+    """A GET request returns the HTML page of the quiz.
+    A POST request has the user's guess inside, or a "give up" message.
+    Giving up is defined as sending the following string: giveup
+    If the guess inside a request was right, the user's score (inside their session cookies) is incremented,
+    and a JSON containing the tag's full name (key: name), path (key: path), relative URL to see the
+    tag (key: rel_path), and new score (key: score) is returned for it to be shown to the player. The same JSON is
+    returned when giving up, the only difference is that the score counter isn't incremented. If the guess is
+    incorrect, the string "incorrect" is returned."""
     if request.method == 'GET':
         tag = display.quiz.random_tag()
         session['tag'] = content.get_folder_path(tag)
@@ -41,15 +49,22 @@ def quiz():
                                d=display.quiz,
                                tag=tag)
     else:
-        tag = session.get('tag', default=None)
-        if tag is None:
-            return 'incorrect'
-        tag = content.find_unit_tag(tag)
-        guess = request.data.decode('utf-8').translate(content.no_punc_trans)
+        tag_path = session.get('tag', default=None)
+        if tag_path is None:
+            return 'error'
+        tag = content.find_unit_tag(tag_path)
+        answer_dict = {'name': tag.name, 'path': tag_path, 'rel_path': flask.url_for('alt_main', tag_path=tag_path),
+                       'score': session['score']}
+
+        guess = request.data.decode('utf-8').translate(content.no_punc_trans).strip()
+
+        if guess == 'giveup':
+            return answer_dict
+
         if guess in tag.alt_names:
-            score = session.get('score', 0)
-            session['score'] = score + 1
-            return tag.name
+            answer_dict['score'] += 1
+            session['score'] = answer_dict['score']
+            return answer_dict
         else:
             return 'incorrect'
 
